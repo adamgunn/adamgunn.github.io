cfc660391b7efcf1e91773ddde19c25923da3c6b
Adam Gunn <adamgunnadamgunn@gmail.com>
2021-08-08 01:05:07 -0500

    title is a random font every time a page is loaded

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Load in the heading for each page
 const heading = `
     <header>
         <ul class="headbar">
@@ -5,29 +6,33 @@ const heading = `
                 <a href=../home><img src=../images/my_face.png class="face" width="100" height="100" alt="Adam's face" /></a>
             </li>
             <li>
-                <a href=../home class="adamGunn">Adam&nbsp;Gunn</a>
+                <a href=../home id="adamGunn">Adam&nbsp;Gunn</a>
             </li>
         </ul>
         <ul class="navbar" id="navbar_links">
-            <li><a href=../home>Home</a></li>
-            <li><a href=../about>About</a></li>
-            <li><a href=../portfolio>Portfolio</a></li>
-            <li><a href=../resume>Resum&eacute;</a></li>
-            <li><a href=../signup>Sign up</a></li>
+            <li><a href=../home class="navbar_text">Home</a></li>
+            <li><a href=../about class="navbar_text">About</a></li>
+            <li><a href=../portfolio class="navbar_text">Portfolio</a></li>
+            <li><a href=../resume class="navbar_text">Resum&eacute;</a></li>
+            <li><a href=../signup class="navbar_text">Sign up</a></li>
         </ul>
     <header
 ` ;
 
 var heading_loaded = false;
-var header; 
-var sticky; 
+var fonts = ['blenny', 'Palette Mosaic', 'rig-solid-light-halftone', 'blackcurrant-squash', 'variex', 'hobeau-rococeaux-background', 'whomp', 'joschmi', 'xants', 'alfarn'];
+var chosen_font = fonts[Math.floor(Math.random() * fonts.length)];
 
 window.addEventListener('DOMContentLoaded', () => {
     let page_header = document.querySelector('heading');
     page_header.innerHTML = heading;
     heading_loaded = true;
+    document.getElementById('adamGunn').style.fontFamily = chosen_font;
 });
 
+// Sticky header
+var header; 
+var sticky; 
 window.onscroll = function() {stickyHeader()};
                         
 function stickyHeader() {

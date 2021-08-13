6007de8a2168fce1c7668bf79c877d2bf7dc9763
Adam Gunn <adamgunnadamgunn@gmail.com>
2021-08-12 21:43:55 -0500

    fixing php issues

--- forms/form-handling.php
+++ forms/form-handling.php
@@ -4,26 +4,26 @@
         $family_name = $_POST['family-name'];
         $email = $_POST['email'];
         $fp = fopen('email-list.txt', 'a');
-        fwrite($fp, 'first name: ');
+        fwrite($fp, "first name: ");
         fwrite($fp, $given_name);
-        fwrite($fp, '\nlast name: ');
+        fwrite($fp, "\n" . "last name: ");
         fwrite($fp, $family_name);
-        fwrite($fp, '\nemail: ');
+        fwrite($fp, "\n" . "email: ");
         fwrite($fp, $email);
         fclose($fp);
     }
     if (isset($_POST['message'])) {
         $message = $_POST['message'];
-        $fp = fopen('email_list.txt', 'a');
-        fwrite($fp, '\nmessage: ');
+        $fp = fopen('email-list.txt', 'a');
+        fwrite($fp, "\n" . "message: ");
         fwrite($fp, $message);
-        fwrite($fp, '\n\n');
+        fwrite($fp, "\n" . "\n");
         fclose($fp);
     }
     else {
         $fp = fopen('email_list.txt', 'a');
-        fwrite($fp, '\nmessage: NONE');
-        fwrite($fp, '\n\n');
+        fwrite($fp, "\n" . "message: NONE");
+        fwrite($fp, "\n" . "\n");
         fclose($fp);
     }
 ?>

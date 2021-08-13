8590ac54c6eabb0ae1345bd9174ae35c87781ae0
Adam Gunn <adamgunnadamgunn@gmail.com>
2021-08-13 00:36:19 -0500

    attempting to solve php isses
    also fixed overflow and made form-handling.php more concise

--- forms/form-handling.php
+++ forms/form-handling.php
@@ -3,29 +3,14 @@
         $given_name = $_POST['given-name'];
         $family_name = $_POST['family-name'];
         $email = $_POST['email'];
-        $fp = fopen('email-list.txt', 'a');
-        fwrite($fp, "first name: ");
-        fwrite($fp, $given_name);
-        fwrite($fp, "\n" . "last name: ");
-        fwrite($fp, $family_name);
-        fwrite($fp, "\n" . "email: ");
-        fwrite($fp, $email);
-        fclose($fp);
-    }
-    if (isset($_POST['message'])) {
         $message = $_POST['message'];
         $fp = fopen('email-list.txt', 'a');
-        fwrite($fp, "\n" . "message: ");
-        fwrite($fp, $message);
-        fwrite($fp, "\n" . "\n");
+        fwrite($fp, "first name: " . $given_name);
+        fwrite($fp, "\n" . "last name: " . $family_name);
+        fwrite($fp, "\n" . "email: " . $email);
+        fwrite($fp, "\n" . "message: " . $message);
+        fwrite($fp, "\n\n");
         fclose($fp);
-    }
-    else {
-        $fp = fopen('email_list.txt', 'a');
-        fwrite($fp, "\n" . "message: NONE");
-        fwrite($fp, "\n" . "\n");
-        fclose($fp);
-    }
 
     function alertSubmit() {
         echo "
@@ -46,4 +31,5 @@
     else {
         header("Location: http://adamgunn.net/sign-up");
     }
+
 ?>
